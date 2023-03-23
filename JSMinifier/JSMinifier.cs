a9e9d5c8a0e7f69a4bab7f4ae12e8d88dc7d8f77
Christoph Döllinger <christoph.doellinger@med.uni-heidelberg.de>
2023-03-23 08:08:51 +0100

    init

---
+++ JSMinifier/JSMinifier.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SqlServer.Server;
+using Yahoo.Yui.Compressor;
+
+namespace JSMinifier
+{
+    public class JSMinifier
+    {
+        [SqlFunction]
+        public static string Minify(string jsCode)
+        {
+            if (jsCode == null)
+                return "";
+
+            var jsCompressor = new JavaScriptCompressor();
+            return jsCompressor.Compress(jsCode);
+        }
+    }
+}
